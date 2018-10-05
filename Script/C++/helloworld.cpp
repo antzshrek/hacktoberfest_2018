111b117f7fb44539635efdfd91768c15b1841832
Antz Shrek <antzshrek@localhost.localdomain>
2018-10-05 11:32:08 +0100

    added more languages

---
+++ Script/C++/helloworld.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// AUTHOR: Reuben Antz
+// GITHUB: https://github.com/antzshrek
+
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "\nHello World, Welcome to hacktoberfest 2018!\n";
+}
---
+++ Script/Python/helloworld.py
@@ -0,0 +1,7 @@
+# LANGUAGE: C++
+# AUTHOR: Reuben Antz
+# GITHUB: https://github.com/antzshrek
+
+
+
+print("Hello World, Welcome to hacktoberfest 2018!")
